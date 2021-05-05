e71f3c9c3085bb56692de4051f01bada750c13e1
Redant136 <43812447+Redant136@users.noreply.github.com>
2021-05-05 16:49:31 -0400

    setup of basic stuff

--- CallOfRizan.cpp
+++ CallOfRizan.cpp
@@ -1,7 +1,37 @@
-#include <kNgine/kNgine.hpp>
+#include "CallOfRizan.hpp"
 #include <iostream>
 
-int main(){
-  std::cout<<"a"<<std::endl;
+kNgine::Camera *MainCamera = new kNgine::Camera(20, 1920, 1080);
+MainCharacter *character;
+
+int main()
+{
+  character = new MainCharacter();
+  kNgine::window_name = "CallOfRizan";
+  kNgine::window_size = v2(1980, 1080);
+
+  // kNgine::addObject(MainCamera);
+
+  kNgine::addObject(character);
+
+  { //floor
+    kNgine::ComponentGameObject *floor = new kNgine::ComponentGameObject();
+    floor->position = v3(0, -5, 0);
+
+    kNgine::physics::b2PhysicsBodyComponent *floorPhys = new kNgine::physics::b2Rect(floor, 20, 1, 50, 0.5f);
+    floorPhys->setStatic(false);
+    floor->addComponent(floorPhys);
+
+    kNgine::SpriteComponent *floorSpr = new kNgine::SpriteComponent(floor, kNgine::fillSprite(200, 200, {0, 255, 0, 255}));
+    floorSpr->spriteDimension = v2(20, 1);
+    floor->addComponent(floorSpr);
+
+    kNgine::addObject(floor);
+  }
+
+  kNgine::addObject(new kNgine::physics::b2PhysicsEngine(v2(0,-1)));
+
+  kNgine::start();
+  kNgine::cleanup();
   return 0;
 }
---
+++ CallOfRizan.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#define kNgine_DEBUG
+#include <kNgine/kNgine.hpp>
+
+class MainCharacter;
+extern kNgine::Camera *MainCamera;
+extern MainCharacter *character;
+
+class MainCharacter : public kNgine::ComponentGameObject
+{
+public:
+  kNgine::SpriteMap *characterSprites;
+  kNgine::RenderableSystem *spriteSystem;
+  MainCharacter()
+  {
+    this->addComponent(new kNgine::NodeObjectComponent(this, MainCamera));
+    characterSprites = new kNgine::SpriteMap();
+    spriteSystem = new kNgine::RenderableSystem(this);
+    spriteSystem->addSprite(new kNgine::RendererObject(this, new kNgine::SpriteAnimation(this, characterSprites, kNgine::importSpriteSheet("mcSpriteSheet.png", 50, 37), 1.f / 16)), "main");
+    characterSprites->offsetPixelsInSprites(NORTH,1);
+
+    this->addComponent(new kNgine::physics::b2Rect(this));
+
+    this->addComponent(spriteSystem);
+    kNgine::addObject(characterSprites);
+  }
+};
